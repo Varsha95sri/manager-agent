1b555dd8edaa2d83e73ba009d34b6d574be86ea2
Varsha Srivastava <varshasrivastava2004@gmail.com>
2026-06-09 17:34:51 +0530

    Configure HTTPS and TrustProxies for SSL termination

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,5 +17,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         \Illuminate\Pagination\Paginator::useBootstrapFive();
+
+        if (config('app.env') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
